bd32db837a742d5145f56f3b7215d9d8438a7261
GayathriVasanthan <gayathrivasanthan0609@gmail.com>
2026-03-09 10:38:57 +0530

    UC3 :Room Inventory

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,2 +1,45 @@
+import java.util.*;
+
+class RoomInventory {
+    private Map<String, Integer> inventory;
+
+    RoomInventory() {
+        inventory = new HashMap<>();
+    }
+
+    public void addRoomType(String type, int count) {
+        inventory.put(type, count);
+    }
+
+    public int getAvailability(String type) {
+        return inventory.getOrDefault(type, 0);
+    }
+
+    public void updateAvailability(String type, int count) {
+        inventory.put(type, count);
+    }
+
+    public void displayInventory() {
+        for (Map.Entry<String, Integer> entry : inventory.entrySet()) {
+            System.out.println(entry.getKey() + " Rooms Available: " + entry.getValue());
+        }
+    }
+}
+
 public class BookMyStay {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking System v3.1");
+
+        RoomInventory inventory = new RoomInventory();
+        inventory.addRoomType("Single", 5);
+        inventory.addRoomType("Double", 3);
+        inventory.addRoomType("Suite", 2);
+
+        inventory.displayInventory();
+
+        inventory.updateAvailability("Single", 4);
+        System.out.println("After booking one Single Room:");
+        inventory.displayInventory();
+    }
 }
